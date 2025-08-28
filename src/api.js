233879454bd5b580d8e66f5316558e3983a6ddb0
Farzan Ullah <79374951+Farzan-Ullah@users.noreply.github.com>
2025-08-28 17:10:26 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://peelncraft-website-server.onrender.com'
+  baseURL: 'https://peelncraft-website-server.onrender.com/api'
 });
 
 // Attach token if present
