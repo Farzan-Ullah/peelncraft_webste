07929354c7d2405a8bc61ef820604a2f4c6ef9a6
Farzan Ullah <farzanullah07@gmail.com>
2025-08-28 16:58:26 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173, proxy: { '/api': 'http://localhost:5000' } }
-})
+  server: {
+    port: 5173,
+    proxy: { "/api": "https://peelncraft-website-server.onrender.com" },
+  },
+});
